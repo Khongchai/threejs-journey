f524f2c82ab5987074593f84723ca4d7cf37c169
khongchai <world1955@hotmail.com>
2021-06-26 20:10:02 +0700

    got to rotation

--- c3l6/src/shaders/test/fragment.glsl
+++ c3l6/src/shaders/test/fragment.glsl
@@ -4,10 +4,25 @@ uniform float uTime;
     uv values go from 0.0 at the bottom left to 1.0 at the top right.
 */
 varying vec2 vUv;
+const float PI = 3.1415926535897932384626433832795;
 
 float random(vec2 st)
 {
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    //where mid is only for calibration.
+    /*
+        Why are we using cos and sin for rotation?
+        First off, it's a formula that is generally used for rotating matrices: https://www.khanacademy.org/computing/pixar/sets/rotation/v/sets-8
+        
+    */
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
 }
 
 void main()
@@ -151,9 +166,15 @@ void main()
         float strength =  0.15 / (distance((vec2(vUv.x, (vUv.y-0.5) * 5.0 + 0.5)), vec2(0.5)));
         gl_FragColor = vec4(vec3(strength), 1.0);      
 
+        Like above, but we combine with multiplication the y and x axes to make a star shape.
+        float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+        strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+        gl_FragColor = vec4(vec3(strength), 1.0); 
     */
 
-    float strength =  0.015 / distance(vec2(vUv.x, ((vUv.y - 0.5) * 5.0) + 0.5), vec2(0.5));
+    vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+    float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
     gl_FragColor = vec4(vec3(strength), 1.0);      
     
 
