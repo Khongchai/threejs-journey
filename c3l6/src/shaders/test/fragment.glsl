6bcc4ce26c539ebae1ce495e10dfadfb25c78f78
khongchai <world1955@hotmail.com>
2021-07-11 13:13:53 +0700

    added a variation

--- c3l6/src/shaders/test/fragment.glsl
+++ c3l6/src/shaders/test/fragment.glsl
@@ -343,12 +343,43 @@ void main()
 
         gl_FragColor = vec4(vec3(mixedColor), 1.0);
 
+        Nice flame
+        float fireShape = mix(0.1, 2.0, vUv.y);
+        float fireStrength = mix(0.01, 0.1, vUv.y);
+        float sineFlameVariation = sin(uTime * 0.8) * 5.0;
+        float cosFlameVariation = cos(uTime * 0.78) * 5.0;
+        float windX = uMouse.x * 0.6;
+        float windY = uMouse.y * 0.8;
+        vec2 wavedUv = vec2(
+            vUv.x + sin(vUv.y * (windX - sineFlameVariation)* fireShape ) * fireStrength,
+            vUv.y + cos(vUv.x * (windY - cosFlameVariation) * fireShape ) * fireStrength
+        );
+        
+         float reduceRadius = 0.25;
+        float strength = 1.0 - step(0.02, abs(distance(wavedUv, vec2(0.5)) - reduceRadius));
+
+        vec3 blackColor = vec3(0.0);
+        float r = mix(0.8, 0.976, vUv.x );
+        float g = mix(0.49, 0.624, vUv.y);
+        float b = mix(0.72, 0.3, vUv.y);
+
+        vec3 mixedColor = mix(blackColor, vec3(r, g, b), strength);
+
+        gl_FragColor = vec4(vec3(mixedColor), 1.0);
+
         */
 
+        float fireShape = mix(0.1, 2.0, vUv.y);
+        float fireStrength = mix(0.01, 0.1, vUv.y);
+        float sineFlameVariation = sin(uTime * 0.8) * 5.0;
+        float cosFlameVariation = cos(uTime * 0.78) * 5.0;
+        float windX = uMouse.x * 0.6;
+        float windY = uMouse.y * 0.8;
         vec2 wavedUv = vec2(
-            vUv.x + sin(vUv.y * uMouse.x * 0.59) * 0.1,
-            vUv.y + cos(vUv.x * uMouse.y * 0.4) * 0.1
+            vUv.x + sin(vUv.y * (windX - sineFlameVariation)* fireShape ) * fireStrength,
+            vUv.y + cos(vUv.x * (windY - cosFlameVariation) * fireShape ) * fireStrength
         );
+        
          float reduceRadius = 0.25;
         float strength = 1.0 - step(0.02, abs(distance(wavedUv, vec2(0.5)) - reduceRadius));
 
